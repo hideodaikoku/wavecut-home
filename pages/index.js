62bab5fa8bab7635504f8a38c9153cb23ee0f060
Hideo Daikoku <neildaikoku@gmail.com>
2023-05-02 17:51:17 -0400

    Revert "redo header"
    This reverts commit bd2e978e1b542d3a5462805f1fd100b6adeacb3f.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
-import Layout from '../components/layout'
+import Layout, {siteTitle} from '../components/layout'
 import Top from '../components/top';
 
 export default function Home() {
   return (
     <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
       <Top home/>
     </Layout>
   )
