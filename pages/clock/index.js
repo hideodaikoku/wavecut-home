85c786b4a8551b34d31c5972d435101a6a53f168
Hideo Daikoku <neildaikoku@gmail.com>
2023-05-02 17:51:28 -0400

    Revert "update dependencies and modify clock"
    This reverts commit 2b17c0fc2d2f19d2184acbc68e09177fc0258a89.

--- pages/clock/index.js
+++ pages/clock/index.js
@@ -1,51 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Canvas } from "@react-three/fiber";
-import styles from '../../styles/canvas.module.scss';
-
-import { Bloom, EffectComposer, LUT } from '@react-three/postprocessing'
-
-import Layout from '../../components/layout'
 import Header from '../../components/header';
-import Controls from './orbitcontrols';
+import { Canvas } from "@react-three/fiber";
+import Layout from '../../components/layout'
 
-
-import Torus from './torus';
 export const siteTitle = "WaveCut Studios | Home";
 
 export default function Clock() {
-    const [time, setTime] = useState(new Date());
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
-
-
-
     return (
         <div className="flex flex-col h-screen">
             <Header siteTitle={siteTitle} />
             <Layout>
                 <div className="w-full h-full">
-                    <Canvas className={styles.canvas}
-                        shadows
-                        camera={{
-                            position: [-6, 7, 7],
-                        }}
-                    >
-                        <ambientLight color={"white"} intensity={0.7} />
-                        <Controls />
-                        <Torus rotateX={0} rotateY={0.2} scale={20} />
-                    </Canvas>
+                <Canvas
+                    shadows
+                    camera={{
+                        position: [-6, 7, 7],
+                    }}
+                >
 
-                    <div className='absolute bottom-20 right-10'>
-                        <p className='text-4xl'>
-                            {time.toLocaleTimeString()}
-                        </p>
-                    </div>
+                </Canvas>
                 </div>
-
+                
             </Layout>
         </div>
     )
