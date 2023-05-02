bd2e978e1b542d3a5462805f1fd100b6adeacb3f
Hideo Daikoku <neildaikoku@gmail.com>
2023-05-02 17:07:26 -0400

    redo header

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
-import Layout, {siteTitle} from '../components/layout'
+import Layout from '../components/layout'
 import Top from '../components/top';
 
 export default function Home() {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
       <Top home/>
     </Layout>
   )
