b52518f8411e06a944287cbc51bfbf50cf962165
Hideo Daikoku <neildaikoku@gmail.com>
2023-05-02 17:19:27 -0400

    remove clock component

--- pages/clock/index.js
+++ pages/clock/index.js
@@ -4,16 +4,12 @@ import styles from '../../styles/canvas.module.scss';
 import Layout from '../../components/layout'
 import Header from '../../components/header';
 import Controls from './orbitcontrols';
-
+import Clock from 'react-live-clock'
 
 import Torus from './torus';
 export const siteTitle = "WaveCut Studios | Home";
 
-export default function Clock() {
-
-
-
-
+export default function WaveClock() {
     return (
         <div className="flex flex-col h-screen">
             <Header siteTitle={siteTitle} />
@@ -29,9 +25,7 @@ export default function Clock() {
                         <Controls />
                         <Torus rotateX={0} rotateY={0.2} scale={20} />
                     </Canvas>
-
                 </div>
-
             </Layout>
         </div>
     )
