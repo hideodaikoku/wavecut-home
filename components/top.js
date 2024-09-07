d0fe2564173d2fe4c8e04040b7d8b6b98bf645bd
Hideo <neildaikoku@gmail.com>
2024-09-07 13:53:45 +0900

    update site

--- components/top.js
+++ components/top.js
@@ -8,29 +8,8 @@ const Top = () => {
                 <h1 className={utilStyles.heading2Xl}>
                     WAVECUT STUDIOS
                 </h1>
-                <p>
-                    Wavecut Studios is a software company based in Tokyo, Japan.
-                    We are currently developing products to automate creative workflows.
-                    Wavecut Studios was founded by
-                    <span> </span>
-                    <a
-                        href="https://www.hideodaikoku.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Hideo Daikoku's Homepage"
-                    >
-                        Hideo Daikoku
-                    </a>
-                    <span> and </span>
-                    <a
-                        href="https://www.linkedin.com/in/jorel-chan-968854159/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Jorel Chan's LinkedIn Page"
-                    >
-                        Jorel Chan
-                    </a>
-                    <span> in 2022. </span>
+                <p className={utilStyles.paragraph}>
+                    Slow computing for a fast world. Currently sailing. &copy; 2024
                 </p>
         </div>
     );
